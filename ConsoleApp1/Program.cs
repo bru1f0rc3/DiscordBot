bb4a35bc2e0eccbcb75fa960aeee8e8c96c61f9f
___ <___@DESKTOP-VP56NTU>
2024-02-11 10:13:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            List<string> playerIds = await GetPlayerIdsFromLatestMatches();
+            List<string> playerNicknames = await GetPlayerNicknames(playerIds);
+
+            Console.WriteLine("Никнеймы игроков из матчей EGGWARS:");
+            foreach (string nickname in playerNicknames)
+            {
+                Console.WriteLine(nickname);
+            }
+        }
+
+        private static async Task<List<string>> GetPlayerIdsFromLatestMatches()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "https://api.vimeworld.ru/match/latest?count=100";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                dynamic result = JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+                List<string> playerIds = new List<string>();
+                foreach (var match in result)
+                {
+                    if (match.game.ToString() == "EGGWARS")
+                    {
+                        string matchId = match.id.ToString();
+                        List<string> matchPlayerIds = await GetPlayerIdsFromMatch(matchId);
+                        playerIds.AddRange(matchPlayerIds);
+                    }
+                }
+
+                return playerIds;
+            }
+        }
+
+        private static async Task<List<string>> GetPlayerIdsFromMatch(string matchId)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = $"https://api.vimeworld.ru/match/{matchId}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                dynamic result = JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+                List<string> playerIds = new List<string>();
+                foreach (var team in result.teams)
+                {
+                    foreach (var memberId in team.members)
+                    {
+                        playerIds.Add(memberId.ToString());
+                    }
+                }
+
+                return playerIds;
+            }
+        }
+
+        private static async Task<List<string>> GetPlayerNicknames(List<string> playerIds)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string baseUrl = "https://api.vimeworld.ru/user/";
+
+                List<string> playerNicknames = new List<string>();
+                foreach (string playerId in playerIds)
+                {
+                    string url = baseUrl + playerId;
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    dynamic result = JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+                    await Task.Delay(500);
+
+                    string nickname = result[0].username.ToString();
+                    playerNicknames.Add(nickname);
+                }
+
+                return playerNicknames;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Team
+    {
+        public string Id { get; set; }
+        public List<int> Members { get; set; }
+        public int Kills { get; set; }
+        public int EggHealth { get; set; }
+    }
+}
